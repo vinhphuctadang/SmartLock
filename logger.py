33e2e4c00c8b6ae00ad15323bd711807854672b9
foo <vinhphuctadang@gmail.com>
2020-10-11 12:36:16 +0700

    update logger to print function

--- logger.py
+++ logger.py
@@ -3,16 +3,10 @@ import sys
 import json
 import traceback
 
-def debug(s):
+def debug(*args, **kargs):
     meta = traceback.format_stack()[0].split('\n')[0].strip().replace('File', '', 1).replace(', line ', ':', 1)
-    if type(s) == type({}):
-        s = json.dumps(s, indent=2)
-    sys.stdout.write('\033[34m[DEBUG,%s]\033[39m %s\n' % (meta, s))
+    print('\033[34m[DEBUG,%s]\033[39m' % meta, *args, file=sys.stdout, **kargs)
 
-def error(s):
-
+def error(*args, **kargs):
     meta = traceback.format_stack()[0].split('\n')[0].strip().replace('File', '', 1).replace(', line ', ':', 1)
-    if type(s) == type({}):
-        s = json.dumps(s, indent=2)
-    sys.stderr.write('\033[31m[ERROR,%s]\033[39m %s\n' % (meta, s))
-    
+    print('\033[31m[ERROR,%s]\033[39m' % meta, *args, file=sys.stderr, **kargs)
