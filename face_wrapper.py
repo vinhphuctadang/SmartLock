8a4f132c0d1fd05b77c3e845ba543e6882ca2832
Tinh Dao <dcongtinh@gmail.com>
2020-10-24 20:38:28 +0700

    update face_wrapper (evaluate with test set)

--- face_wrapper.py
+++ face_wrapper.py
@@ -1,11 +1,13 @@
 # import modules
 import tensorflow
-from tensorflow import keras 
-import logger 
+from tensorflow import keras
+import logger
 import os
 import time
-import numpy as np 
+import numpy as np
 import json
+import matplotlib.pyplot as plt
+
 
 class FaceWrapper:
     """FaceWrapper model is responsible for interventing preprocessing and training steps for experiments
@@ -16,6 +18,7 @@ class FaceWrapper:
         # configs goes here
         self.faceWidth = 128
         self.faceHeight = 128
+        self.faceChannel = 3
         self.batchSize = 32
         self.datasetDir = './faces'
         self.saveDir = './models'
@@ -30,79 +33,120 @@ class FaceWrapper:
 
         # we might want to override these function to intervent steps for researching
         # steps of using these callback please refer to './train.py' for details
-        self._import_dataset = None 
-        self._preprocess = None 
-        self._build_model = None 
+        self._import_dataset = None
+        self._preprocess = None
+        self._build_model = None
         self._train = None
-        self._predict = None 
+        self._predict = None
 
     def importDataset(self):
 
         if self._import_dataset:
             return self._import_dataset(self)
         # preprocess training data
-        train_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255, validation_split=1./3)
+        train_datagen = keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255, validation_split=1./3)
+        test_datagen = keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255, validation_split=1./3)
 
         # split dataset
         train_set = train_datagen.flow_from_directory(
             self.datasetDir,
             seed=10,
             target_size=(self.faceHeight, self.faceWidth),
-            class_mode='categorical', 
-            batch_size=self.batchSize, 
+            class_mode='categorical',
+            color_mode='rgb' if self.faceChannel == 3 else 'grayscale',
+            batch_size=self.batchSize,
             subset="training",
         )
-
-        test_set = train_datagen.flow_from_directory(
+        valid_set = train_datagen.flow_from_directory(
             self.datasetDir,
             seed=10,
             target_size=(self.faceHeight, self.faceWidth),
-            class_mode='categorical', 
-            batch_size=self.batchSize, 
+            class_mode='categorical',
+            color_mode='rgb' if self.faceChannel == 3 else 'grayscale',
+            batch_size=self.batchSize,
             subset="validation"
         )
 
-        return train_set, test_set
+        test_set = test_datagen.flow_from_directory(
+            self.datasetDir,
+            seed=10,
+            target_size=(self.faceHeight, self.faceWidth),
+            class_mode='categorical',
+            color_mode='rgb' if self.faceChannel == 3 else 'grayscale',
+            batch_size=1,
+            subset="validation"
+        )
+
+        return train_set, valid_set, test_set
 
     def preprocess(self, X):
         if self._preprocess:
             return self._preprocess(self, X)
-        return X 
+        return X
 
     def buildModel(self):
         if self._build_model:
             return self._build_model(self)
 
         model = keras.Sequential([
-            keras.layers.Flatten(input_shape=(self.faceHeight, self.faceWidth, 3)),
+            keras.layers.Flatten(input_shape=(
+                self.faceHeight, self.faceWidth, self.faceChannel)),
             keras.layers.Dense(32, activation='relu'),
             keras.layers.Dropout(0.2),
             keras.layers.Dense(2, activation='softmax'),
         ])
-        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        model.compile(optimizer='adam',
+                      loss='categorical_crossentropy', metrics=['accuracy'])
         return model
 
-    def runWorkFlow(self, epochs=20): # exec the whole workflow
-        train, test = self.importDataset()
+    def runWorkFlow(self, epochs=20):  # exec the whole workflow
+        train, valid, test = self.importDataset()
         train = self.preprocess(train)
-        test = self.preprocess(test)
+        valid = self.preprocess(valid)
         self.model = self.buildModel()
-        # logger.debug(train, test)
-        self.train(train, test, epochs=epochs)
+        # logger.debug(train, valid)
+        self.train(train, valid, test, epochs=epochs)
         return self
-        
-    def train(self, train_set, test_set, epochs=20):
+
+    def plot_fig(self, epoch_arr, name, filepath, train, val):
+        plt.plot(epoch_arr, train, 'g-o', label='Training ' + name)
+        plt.plot(epoch_arr, val, 'r-o', label='Validation ' + name)
+        plt.title('Testing Accuracy & ' + name)
+        plt.legend()
+        plt.xlabel("Epochs")
+        plt.ylabel(name)
+        plt.savefig(filepath)
+        plt.clf()
+
+    def train(self, train_set, valid_set, test_set, epochs=20):
 
         if self._train:
-            self.history = self._train(self, train_set, test_set)
-        else: 
+            self.history = self._train(self, train_set, valid_set)
+        else:
+            time_start = time.time()
+            STEP_SIZE_TRAIN = max(1, train_set.samples //
+                                  train_set.batch_size)
+            STEP_SIZE_VALID = max(1, valid_set.samples //
+                                  valid_set.batch_size)
             self.history = self.model.fit(
-                train_set, # data to train, a format of (X, y)
-                steps_per_epoch=len(train_set), # The number of batch iterations before a training epoch is considered finished. Ignore if whole data is load, in our case, we need this in order to iterate over batches; hence make sure that generator can generate at least: steps_per_epoch * epochs batches
-                epochs=epochs, # number of epochs, i.e times that iteration of updating weights goes
-                validation_data=test_set, # validation
-                validation_steps=len(test_set)
+                train_set,  # data to train, a format of (X, y)
+                # The number of batch iterations before a training epoch is considered finished. Ignore if whole data is load, in our case, we need this in order to iterate over batches; hence make sure that generator can generate at least: steps_per_epoch * epochs batches
+                steps_per_epoch=STEP_SIZE_TRAIN,
+                epochs=epochs,  # number of epochs, i.e times that iteration of updating weights goes
+                validation_data=valid_set,  # validation
+                validation_steps=STEP_SIZE_VALID
             )
+            STEP_SIZE_TEST = max(1, test_set.samples //
+                                 test_set.batch_size)
+            (eval_loss, eval_accuracy) = self.model.evaluate(
+                test_set, steps=STEP_SIZE_TEST, verbose=1)
+            print('Loss = ', eval_loss)
+            print('Accuracy = ', eval_accuracy)
+            print("Total run-time: %f seconds" % (time.time() - time_start))
+            # os.system(
+            #     "say Your experiment has finished. Please collect your result")
         return self
 
     def predict(self, X):
@@ -112,12 +156,12 @@ class FaceWrapper:
 
         predictions = self.model.predict(np.array(X))
         return predictions
-        
+
     def save(self, id):
         directory = os.path.join(self.saveDir, id)
         if not os.path.isdir(directory):
             os.mkdir(directory)
-        
+
         # save model to file
         model_dir = os.path.join(directory, id+'.h5')
         self.model.save(model_dir)
@@ -127,12 +171,26 @@ class FaceWrapper:
         with open(meta_dir, 'w') as f:
             print(json.dumps(self.classes), file=f)
             print(self.history.history, file=f)
+
+        # plot figure after training model
+        epochs = len(self.history.history['accuracy'])
+        epoch_arr = [i for i in range(epochs)]
+        train_acc = self.history.history['accuracy']
+        train_loss = self.history.history['loss']
+
+        val_acc = self.history.history['val_accuracy']
+        val_loss = self.history.history['val_loss']
+
+        self.plot_fig(epoch_arr, 'Accuracy',
+                      os.path.join(directory, id+'_acc.png'), train_acc, val_acc)
+        self.plot_fig(epoch_arr, 'Loss', os.path.join(directory, id+'_loss.png'),
+                      train_loss, val_loss)
         return self
 
     def load(self, id):
         directory = os.path.join(self.saveDir, id)
         model_dir = os.path.join(directory, id + '.h5')
-        meta_dir  = os.path.join(directory, id + '.meta')
+        meta_dir = os.path.join(directory, id + '.meta')
 
         self.model = keras.models.load_model(model_dir)
 
@@ -140,7 +198,8 @@ class FaceWrapper:
             classes = f.readline()
             self.classes = json.loads(classes)
             logger.debug("Loaded classes:", self.classes)
-        return self 
+        return self
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     logger.debug('Tensorflow version:', tensorflow.__version__)
--- train.py
+++ train.py
@@ -1,29 +1,38 @@
-import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
-from face_wrapper import FaceWrapper as FW 
+from tensorflow.keras import layers, models
+from face_wrapper import FaceWrapper as FW
+
 
 def buildCNN(self):
-
     """Build a CNN model, the build process is attached to the FaceWrapper workflow
     Returns:
         Keras model
     """
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                            input_shape=(128, 128, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
+
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Flatten())
-    model.add(layers.Dense(16, activation='relu'))
-    model.add(layers.Dense(2, activation='softmax')) # no softmax activation applied, we will use logit
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    # model.add(layers.Dense(16, activation='relu'))
+    # no softmax activation applied, we will use logit
+    model.add(layers.Dense(2, activation='softmax'))
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy', metrics=['accuracy'])
     return model
+
 
 def main():
     face = FW()
     face._build_model = buildCNN
-    face.classes = ['phuc', 'quy']
+    face.classes = ['thao', 'tinh']
 
     # this will run the workflow as describe in flow.png
     face.runWorkFlow(epochs=3).save('sample')
+
+
 main()
