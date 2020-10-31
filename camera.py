46af0a5e86c432da99502fb9a20b8e27dec033fe
Tinh Dao <dcongtinh@gmail.com>
2020-10-31 22:27:31 +0700

    added: some feature extractions

--- camera.py
+++ camera.py
@@ -3,6 +3,7 @@ import cv2
 import logger
 import numpy as np
 from joblib import load
+from skimage import feature
 import face_recognition as fr
 # CONFIG: replace video URI here
 VIDEO_URI = 0
@@ -16,7 +17,8 @@ ratio = FACE_SIZE[0] / TARGET_SIZE[0]
 
 # utils
 fontFace = cv2.FONT_HERSHEY_SIMPLEX
-fontScale, thickness = 1, 2
+fontScale, thickness = 0.75, 2
+threshold = 0.9
 
 
 def extract_features(img):
@@ -27,15 +29,66 @@ def extract_features(img):
         face_bounding_boxes = fr.face_locations(img)
         # If detecting image contains exactly one face
         if len(face_bounding_boxes) == 1:
-            face_enc = fr.face_encodings(img, face_bounding_boxes)
+            feature_vector = fr.face_encodings(img, face_bounding_boxes)
+            # top, right, bottom, left = face_bounding_boxes[0]
+            # roi = img[top:bottom, left:right]
+            # img_ycrcb = cv2.cvtColor(roi, cv2.COLOR_BGR2YCR_CB)
+            # img_luv = cv2.cvtColor(roi, cv2.COLOR_BGR2LUV)
+
+            # ycrcb_hist = calc_hist(img_ycrcb)
+            # luv_hist = calc_hist(img_luv)
+            # feature_vector = np.append(ycrcb_hist.ravel(), luv_hist.ravel())
+            # feature_vector = feature_vector.reshape(1, len(feature_vector))
+            # feature_vector = LBP(img, 24, 8)
             box = np.array(face_bounding_boxes[0])
             box = box * ratio
+
             # box: int required
-            return face_enc, np.array(box, dtype='int64')
+            return feature_vector, np.array(box, dtype='int64')
         else:
             return [], []
     except:
         return [], []
+
+
+def predict(clf, features):
+    label = clf.predict(features)[0]
+    if label == 'unknown':
+        return 'Unknown~'
+
+    proba = clf.predict_proba(features)
+    # print(proba)
+    acc_max = np.max(proba[0])
+    if acc_max < threshold:
+        return 'Unknown~'
+
+    return '%s %.2f' % (label, acc_max*100)
+
+
+def calc_hist(img):
+    histogram = [0] * 3
+    for j in range(3):
+        histr = cv2.calcHist([img], [j], None, [256], [0, 256])
+        histr *= 255.0 / histr.max()
+        histogram[j] = histr
+    return np.array(histogram)
+
+
+def LBP(image, numPoints, radius, eps=1e-7):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # compute the Local Binary Pattern representation
+    # of the image, and then use the LBP representation
+    # to build the histogram of patterns
+    lbp = feature.local_binary_pattern(image, numPoints,
+                                       radius, method="uniform")
+    (hist, _) = np.histogram(lbp.ravel(),
+                             bins=np.arange(0, numPoints + 3),
+                             range=(0, numPoints + 2))
+    # normalize the histogram
+    hist = hist.astype("float")
+    hist /= (hist.sum() + eps)
+    # return the histogram of Local Binary Patterns
+    return np.array([hist])
 
 
 def main():
@@ -60,26 +113,29 @@ def main():
         if process_this_frame:
             features, box = extract_features(frame)
             if len(features) == 1:
-                label = clf.predict(features)[0]
-                # print(clf.predict_proba(features))
                 top, right, bottom, left = box
-                # Draw a box around the face
-                cv2.rectangle(frame, (left, top),
-                              (right, bottom), (0, 255, 0), 2)
+                try:
+                    # Draw a box around the face
+                    cv2.rectangle(frame, (left, top),
+                                  (right, bottom), (0, 255, 0), 2)
 
-                # Draw a label with a name below the face
-                labelSize = cv2.getTextSize(
-                    label, fontFace, fontScale, thickness)[0]
-                cv2.rectangle(frame, (left-1, top),
-                              (left+labelSize[0], top-labelSize[1]-20), (0, 255, 0), cv2.FILLED)
-                cv2.putText(frame, label, (left, top - 10),
-                            fontFace, fontScale=fontScale, color=(0, 0, 0), thickness=thickness)
+                    # Draw a label with a name below the face
+                    label = predict(clf, features)
+                    labelSize = cv2.getTextSize(
+                        label, fontFace, fontScale, thickness)[0]
+                    cv2.rectangle(frame, (left-1, top),
+                                  (left+labelSize[0], top-labelSize[1]-20), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(frame, label, (left, top - 10),
+                                fontFace, fontScale=fontScale, color=(0, 0, 0), thickness=thickness)
+                except:
+                    pass
 
         # # accelerate display frame
         # process_this_frame = not process_this_frame
 
         # Display the resulting image
         cv2.imshow('Video', frame)
+        # cv2.imshow('Video2', roi)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
