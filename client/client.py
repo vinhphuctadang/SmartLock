f2ee11bab9e5a0e835b557c2ff1769483445d500
Vinh Phuc Ta Dang <vinhphuctadang@gmail.com>
2021-01-08 16:43:57 +0700

    update frame save in ui

--- client/client.py
+++ client/client.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 import cv2
+import time
 from PIL import Image, ImageTk
 # from functools import partial
 
 CAMERA_URI = 1
 RECORDING = False
+FRAME_COUNT = 0
+SAVE_INTERVAL = 5
 
 width, height = 300, 300
 cap = cv2.VideoCapture(CAMERA_URI)
@@ -23,7 +26,7 @@ def onRecordClick():
         RECORDING = False
     else:
         RECORDING = True 
-        print('Turn on recording', RECORDING)
+        FRAME_COUNT = 0
         recordButtonText.set('Recording ... (Press to stop)')
 
 root = tk.Tk()
@@ -42,12 +45,18 @@ lmain = tk.Label(root)
 lmain.grid(column=0, row=0)
 
 def show_frame():
-    global RECORDING
+    global RECORDING, FRAME_COUNT
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
     if RECORDING:
-        frame_copy = frame.copy()
-        frame = cv2.rectangle(frame, (0, 0), (frame.shape[1], frame.shape[0]), (0, 255, 0), 5)
+        FRAME_COUNT += 1
+        if FRAME_COUNT % SAVE_INTERVAL == 0:
+            # frame_copy = frame.copy()
+            filename = f'{int(time.time()*1000)}.jpg'
+            print(f'Going to save image as {filename}')
+            cv2.imwrite(filename, frame)
+            FRAME_COUNT = 0
+            frame = cv2.rectangle(frame, (0, 0), (frame.shape[1], frame.shape[0]), (0, 255, 0), 5)
     cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
     img = Image.fromarray(cv2image)
     imgtk = ImageTk.PhotoImage(image=img)
