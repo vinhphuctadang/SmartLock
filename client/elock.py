294e54251f52e46e69be4c61cd087f1ced9cec8d
Vinh Phuc Ta Dang <vinhphuctadang@gmail.com>
2021-01-16 14:48:01 +0700

    add checking lock health

--- client/elock.py
+++ client/elock.py
@@ -1,11 +1,13 @@
 import os 
 is_gpio_env = False 
-if os.environ.get('RASP', None):
+if os.environ.get('GPIO', None):
 	is_gpio_env = True 
 
 if is_gpio_env:
 	import RPi.GPIO as GPIO
-	
+
+import time
+
 def setLock(lockState):
 	print('Going to set lock to state:', lockState)
 	if not is_gpio_env:
@@ -18,5 +20,10 @@ def setLock(lockState):
 	lockState = {True: GPIO.HIGH, False: GPIO.LOW}[lockState]
 	GPIO.output(21,lockState)
 
+# test GPIO
+print('Checking lock health')
 setLock(False)
-
+time.sleep(1)
+setLock(True)
+time.sleep(1)
+setLock(False)
